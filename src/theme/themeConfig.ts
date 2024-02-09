bf51c16103b20070cc6e79a5538ea6070fbd5059
FOLIO3\syedfaranmustafa <syedfaranmustafa@folio3.com>
2024-02-09 16:48:21 +0500

    feat: basic configuration

---
+++ src/theme/themeConfig.ts
@@ -0,0 +1,60 @@
+const theme = {
+  token: {
+    colorSuccess: "#72c040",
+    colorError: "#ec5b56",
+    colorLink: "#101418",
+    borderRadius: 6,
+    colorBgBase: "#f4f4f4",
+    fontSize: "inherit",
+    fontFamily: "'Poppins', sans-serif",
+    wireframe: true,
+    colorPrimary: "#101418",
+    colorInfo: "#3479f7",
+    colorWarning: "#efaf41",
+    colorTextBase: "#6c727f",
+    components: {
+      Button: {
+        defaultColor: "rgb(16, 20, 24)",
+        defaultBorderColor: "rgb(16, 20, 24)",
+        defaultBg: "rgb(229, 230, 235)",
+        fontSize: "inherit",
+        borderRadius: 6,
+        fontWeight: 500
+      },
+      Typography: {
+        fontSizeHeading1: 32,
+        fontSizeHeading2: 22,
+        fontSizeHeading3: 20,
+        fontSizeHeading4: 16,
+        fontSizeHeading5: 14,
+        fontFamilyCode: "'Poppins', sans-serif;",
+        fontFamily: "'Poppins', sans-serif",
+        fontWeightStrong: 700,
+        colorText: "rgb(108, 114, 127)",
+        colorTextDescription: "rgb(108, 114, 127)",
+        colorTextHeading: "rgb(108, 114, 127)"
+      },
+      Input: {
+        activeBorderColor: "rgb(16, 20, 24)",
+        hoverBorderColor: "rgb(16, 20, 24)",
+        colorBorder: "rgb(210, 213, 219)",
+        paddingBlock: 10,
+        controlHeight: 0,
+        fontFamily: "'Poppins', sans-serif;"
+      },
+      Menu: {
+        fontFamily: "inherit",
+        lineType: "",
+        borderRadius: 0,
+        itemBorderRadius: 0,
+        fontSize: "inherit",
+        itemActiveBg: "rgb(16, 20, 24)",
+        itemColor: "rgb(16, 20, 24)",
+        itemHoverColor: "rgb(255, 255, 255)"
+      }
+    },
+    algorithm: "compact"
+  }
+};
+
+export default theme;
