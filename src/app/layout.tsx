086a3eb3215b7bd9b9fbe5980426ded827a8fae0
FOLIO3\syedfaranmustafa <syedfaranmustafa@folio3.com>
2024-02-09 17:06:02 +0500

    feat: setup and eslint and enforced prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@styles/globals.css";
-import "@theme/themeConfig";
-import { Author } from "next/dist/lib/metadata/types/metadata-types";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '@styles/globals.css'
+import '@theme/themeConfig'
+import { Author } from 'next/dist/lib/metadata/types/metadata-types'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Next.js with Windi CSS",
-  description: "Next.js with Windi CSS",
-  viewport: "width=device-width, initial-scale=1",
-  authors: ["SyedFaranMustafa"] as Author[]
-};
+  title: 'Next.js with Windi CSS',
+  description: 'Next.js with Windi CSS',
+  viewport: 'width=device-width, initial-scale=1',
+  authors: ['SyedFaranMustafa'] as Author[],
+}
 
 export default function RootLayout(props: {
-  children: React.ReactNode;
-  params: { locale: string };
+  children: React.ReactNode
+  params: { locale: string }
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{props.children}</body>
     </html>
-  );
+  )
 }
--- src/theme/themeConfig.ts
+++ src/theme/themeConfig.ts
@@ -1,25 +1,25 @@
 const theme = {
   token: {
-    colorSuccess: "#72c040",
-    colorError: "#ec5b56",
-    colorLink: "#101418",
+    colorSuccess: '#72c040',
+    colorError: '#ec5b56',
+    colorLink: '#101418',
     borderRadius: 6,
-    colorBgBase: "#f4f4f4",
-    fontSize: "inherit",
+    colorBgBase: '#f4f4f4',
+    fontSize: 'inherit',
     fontFamily: "'Poppins', sans-serif",
     wireframe: true,
-    colorPrimary: "#101418",
-    colorInfo: "#3479f7",
-    colorWarning: "#efaf41",
-    colorTextBase: "#6c727f",
+    colorPrimary: '#101418',
+    colorInfo: '#3479f7',
+    colorWarning: '#efaf41',
+    colorTextBase: '#6c727f',
     components: {
       Button: {
-        defaultColor: "rgb(16, 20, 24)",
-        defaultBorderColor: "rgb(16, 20, 24)",
-        defaultBg: "rgb(229, 230, 235)",
-        fontSize: "inherit",
+        defaultColor: 'rgb(16, 20, 24)',
+        defaultBorderColor: 'rgb(16, 20, 24)',
+        defaultBg: 'rgb(229, 230, 235)',
+        fontSize: 'inherit',
         borderRadius: 6,
-        fontWeight: 500
+        fontWeight: 500,
       },
       Typography: {
         fontSizeHeading1: 32,
@@ -30,31 +30,31 @@ const theme = {
         fontFamilyCode: "'Poppins', sans-serif;",
         fontFamily: "'Poppins', sans-serif",
         fontWeightStrong: 700,
-        colorText: "rgb(108, 114, 127)",
-        colorTextDescription: "rgb(108, 114, 127)",
-        colorTextHeading: "rgb(108, 114, 127)"
+        colorText: 'rgb(108, 114, 127)',
+        colorTextDescription: 'rgb(108, 114, 127)',
+        colorTextHeading: 'rgb(108, 114, 127)',
       },
       Input: {
-        activeBorderColor: "rgb(16, 20, 24)",
-        hoverBorderColor: "rgb(16, 20, 24)",
-        colorBorder: "rgb(210, 213, 219)",
+        activeBorderColor: 'rgb(16, 20, 24)',
+        hoverBorderColor: 'rgb(16, 20, 24)',
+        colorBorder: 'rgb(210, 213, 219)',
         paddingBlock: 10,
         controlHeight: 0,
-        fontFamily: "'Poppins', sans-serif;"
+        fontFamily: "'Poppins', sans-serif;",
       },
       Menu: {
-        fontFamily: "inherit",
-        lineType: "",
+        fontFamily: 'inherit',
+        lineType: '',
         borderRadius: 0,
         itemBorderRadius: 0,
-        fontSize: "inherit",
-        itemActiveBg: "rgb(16, 20, 24)",
-        itemColor: "rgb(16, 20, 24)",
-        itemHoverColor: "rgb(255, 255, 255)"
-      }
+        fontSize: 'inherit',
+        itemActiveBg: 'rgb(16, 20, 24)',
+        itemColor: 'rgb(16, 20, 24)',
+        itemHoverColor: 'rgb(255, 255, 255)',
+      },
     },
-    algorithm: "compact"
-  }
-};
+    algorithm: 'compact',
+  },
+}
 
-export default theme;
+export default theme
